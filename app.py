77a239799dbb9919a9cf876bfe49a21d911b3d97
Lakshay Gola <lakshaygola@Lakshays-Laptop.local>
2024-08-03 22:36:35 +0530

    logging and return error fixed

--- app.py
+++ app.py
@@ -6,12 +6,16 @@ import requests as rq
 app = Flask(__name__)
 
 headers = {
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Connection": "keep-alive"
 }
 
 def get_price(style_id):
     url = f'https://www.myntra.com/{style_id}'
     res = rq.get(url, headers=headers)
+    print(res.text)
     soup = BeautifulSoup(res.text, 'html.parser')
     
     script_text = next((s.get_text(strip=True) for s in soup.find_all("script") if 'pdpData' in s.text), None)
@@ -23,7 +27,7 @@ def get_price(style_id):
             return mrp, price
         except (json.JSONDecodeError, KeyError):
             pass
-    return 'OOS'
+    return 'OOS', 'OOS'
 
 @app.route('/get_prices', methods=['GET'])
 def get_prices():
